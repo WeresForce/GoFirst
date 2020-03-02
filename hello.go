7565e199312c9d772aa419aa55974f1a9f675013
Anthony <anthony.weres.force@gmail.com>
2020-03-02 12:15:31 -0500

    Two endpoints

--- hello.go
+++ hello.go
@@ -1,68 +1,107 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/view/", viewHandler)
+	// http.HandleFunc("/view/", viewHandler)
 
-	http.HandleFunc("/edit/", editHandler)
+	// http.HandleFunc("/edit/", editHandler)
+
+	http.HandleFunc("/options/", showOptions)
+
+	http.HandleFunc("/showSelectedOption/", showSelectedOption)
 
 	//http.HandleFunc("/save/", saveHandler)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
-type Page struct {
-	Title string
-	Body  []byte
-}
+// type Page struct {
+// 	Title string
+// 	Body  []byte
+// }
 
-func (p *Page) save() error {
-	fileName := p.Title + ".txt"
-	return ioutil.WriteFile(fileName, p.Body, 0600)
-}
+func showOptions(writer http.ResponseWriter, request *http.Request) {
+	jsonFile, err := os.Open("options.json")
+	jsonByteValue, _ := ioutil.ReadAll(jsonFile)
 
-func loadPage(title string) (*Page, error) {
-	fileName := title + ".txt"
-	body, error := ioutil.ReadFile(fileName)
-	if error != nil {
-		return nil, error
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func editHandler(writer http.ResponseWriter, request *http.Request) {
-	title := request.URL.Path[len("/edit/"):]
-	p, err := loadPage(title)
 	if err != nil {
-		p = &Page{Title: title}
+		fmt.Println(err)
 	}
-	t, _ := template.ParseFiles("edit.html")
-	t.Execute(writer, p)
 
-	// fmt.Fprintf(writer, "<h1>Editing %s</h1>"+
-	// 	"<form action = \"/save/%s\" method =\"POST\">"+
-	// 	"<textarea namne==\"body\"> %s</textarea><br>"+
-	// 	"<input type = \"submit\" value=\"Save\">"+
-	// 	"</form>",
-	// 	p.Title, p.Title, p.Body)
+	defer jsonFile.Close()
+
+	type Options struct {
+		OptionOne   string
+		OptionTwo   string
+		OptionThree string
+	}
+
+	var optionsarray Options
+
+	json.Unmarshal(jsonByteValue, &optionsarray)
+
+	template, _ := template.ParseFiles("chooseOption.html")
+
+	template.Execute(writer, optionsarray)
+
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	page, _ := loadPage(title)
+func showSelectedOption(writer http.ResponseWriter, request *http.Request) {
 
-	t, _ := template.ParseFiles("view.html")
-	t.Execute(w, page)
-	//fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
+	type SelectedOptions struct {
+		SelectedRadiobutton string
+		AdditionalInfo      string
+	}
+
+	myOptions := SelectedOptions{SelectedRadiobutton: request.FormValue("chooseHero"),
+		AdditionalInfo: request.FormValue("Thename")}
+
+	template, _ := template.ParseFiles("showSelectedOption.html")
+	template.Execute(writer, myOptions)
 }
 
-func handler(responceWriter http.ResponseWriter, request *http.Request) {
-	fmt.Println(responceWriter, "Hi there, I love %s!", request.URL.Path[1:])
-}
+// func (p *Page) save() error {
+// 	fileName := p.Title + ".txt"
+// 	return ioutil.WriteFile(fileName, p.Body, 0600)
+// }
+
+// func loadPage(title string) (*Page, error) {
+// 	fileName := title + ".txt"
+// 	body, error := ioutil.ReadFile(fileName)
+// 	if error != nil {
+// 		return nil, error
+// 	}
+// 	return &Page{Title: title, Body: body}, nil
+// }
+
+// func editHandler(writer http.ResponseWriter, request *http.Request) {
+// 	title := request.URL.Path[len("/edit/"):]
+// 	p, err := loadPage(title)
+// 	if err != nil {
+// 		p = &Page{Title: title}
+// 	}
+// 	t, _ := template.ParseFiles("edit.html")
+// 	t.Execute(writer, p)
+
+// }
+
+// func viewHandler(w http.ResponseWriter, r *http.Request) {
+// 	title := r.URL.Path[len("/view/"):]
+// 	page, _ := loadPage(title)
+
+// 	t, _ := template.ParseFiles("view.html")
+// 	t.Execute(w, page)
+// }
+
+// func handler(responceWriter http.ResponseWriter, request *http.Request) {
+// 	fmt.Println(responceWriter, "Hi there, I love %s!", request.URL.Path[1:])
+// }
